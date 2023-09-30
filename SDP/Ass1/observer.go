45f21d4068bab9cdd36cf0020e21b1096d08339f
murat <muratkairbek908@gmail.com>
2023-09-30 23:58:08 +0600

    Observer is done and tested

--- SDP/Ass1/observer.go
+++ SDP/Ass1/observer.go
@@ -40,10 +40,30 @@ type patient struct {
 	ID uint 
 }
 
-func (p *patient) notify(doctorFullName string, doctorSpecification string){fmt.Println("'doctor %s %s is now has entry!' email sended to patient %s with id: %d", doctorFullName, doctorSpecification, p.name + " " + p.surname, p.ID)
-	fmt.Println("'doctor %s %s is now has entry!' email sended to patient %s with id: %d", doctorFullName, doctorSpecification, p.name + " " + p.surname, p.ID)
+func (p *patient) notify(doctorFullName string, doctorSpecification string) {	
+	fmt.Printf("'doctor %s %s is now has entry!' email sended to patient %s with id: %d\n", doctorFullName, doctorSpecification, p.name + " " + p.surname, p.ID)
 }
 
 func main(){
+	pat1 := patient{"Maksat", "Zhalkysbaev", 132465}
+	pat2 := patient{"Zhalgas", "Konebayev", 123423}
+	pat3 := patient{"Maxim", "Zaycev", 345123}
+
+	doc1 := therapist{"Kanat", "Kairanas", make([]patient, 0, 10)}
 	
+	doc1.addPatient(pat1)
+	doc1.addPatient(pat2)
+
+	fmt.Println(pat3)
+	doc1.notifyPatients()
+	fmt.Println()
+
+
+	doc1.deletePatient(pat2)
+	
+	doc1.addPatient(pat3)
+	fmt.Println(doc1.patients)
+	doc1.notifyPatients()
+
+
 }
